a49a360c2d56fd0b036afe541a0625d5a5c5e846
Gamy Aguirre <gamyag@gmail.com>
2023-05-13 15:19:53 -0600

    add database and model create

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 // middleware
 app.use((req, res, next) => {
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
     res.json({mssg: 'welcome'})
 })
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log('listening on port 4000')
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT || 4000, () => {
+            console.log('listening on port 4000')
+        })
+    })
+    .catch((error)=> {
+        console.log(error)
+    })
