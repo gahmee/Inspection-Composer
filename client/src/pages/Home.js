9c9886f79c41f27b99400a73e84dc3c562d2f6ea
Gamy Aguirre <gamyag@gmail.com>
2023-05-20 15:37:48 -0600

    add data fetch

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Lists } from "../components/Lists"
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Lists/>
+    </div>
+  )
+}
+
+export default Home
