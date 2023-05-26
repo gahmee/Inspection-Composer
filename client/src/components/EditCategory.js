673d3ad716ece16b7e8c36a10b702134765be822
Gamy Aguirre <gamyag@gmail.com>
2023-05-25 18:50:36 -0600

    add handle input change

--- client/src/components/EditCategory.js
+++ client/src/components/EditCategory.js
@@ -1,12 +1,27 @@
 import { useState } from "react"
 
 const EditCategory = ({category, handleToggleEdit}) => {
-    const [listItem, setListItem] = useState(category)
+    const [list, setList] = useState(category.list)
+
+    const handleInputChange = (value, index) => {
+        const updatedList = [...list]
+        updatedList[index] = value
+        
+        setList([...updatedList])
+    }
 
     return (
         <div>
             <h1>Editing {category.title}</h1>
-            {category.list.map((i) => <div>{i}</div>)}
+            {list.map((item, index) => 
+                <div>
+                    <input 
+                        type="text" 
+                        value={list[index]} 
+                        onChange={(e) => handleInputChange(e.target.value, index)}>
+                    </input>
+                </div>
+            )}
             <button onClick={(event) => handleToggleEdit(event, category)}>Cancel</button>
         </div>
     )
