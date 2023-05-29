b08dcc540aa7c8ec7e70ce4780ebd909ecfdbc22
Gamy Aguirre <gamyag@gmail.com>
2023-05-29 14:02:14 -0600

    add handle submit

--- client/src/components/EditCategory.js
+++ client/src/components/EditCategory.js
@@ -1,13 +1,34 @@
 import { useState } from "react"
 
 const EditCategory = ({category, handleToggleEdit}) => {
+    const [title, setTitle] = useState(category.title)
     const [list, setList] = useState(category.list)
 
     const handleInputChange = (value, index) => {
         const updatedList = [...list]
         updatedList[index] = value
-        
+
         setList([...updatedList])
+    }
+
+    const handleSubmit = async (event) => {
+
+
+        const updatedCategory = {title, list}
+
+        const response = await fetch('http://localhost:4000/api/inspection/' + category._id, {
+            method: 'PATCH',
+            body: JSON.stringify(updatedCategory),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const json = await response.json()
+
+        if (response.ok) {
+            console.log('edited new product', json)
+        }
     }
 
     return (
@@ -22,6 +43,7 @@ const EditCategory = ({category, handleToggleEdit}) => {
                     </input>
                 </div>
             )}
+            <button onClick={(event) => handleSubmit(event)}>Submit</button>
             <button onClick={(event) => handleToggleEdit(event, category)}>Cancel</button>
         </div>
     )
