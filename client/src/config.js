e5aa49cf8f634f997d4d434c7b42ae25dbc9b75d
Gamy Aguirre <gamyag@gmail.com>
2023-06-15 18:35:49 -0600

    update prod url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 const dev = 'http://localhost:4000/api/inspection/';
-const prod = 'https://inspection-composer.onrender.com/';
+const prod = 'https://inspection-composer.onrender.com/api/inspection/';
 
 export const API_URL = process.env.NODE_ENV === 'development' ? dev : prod;
